79c45609bb536ce8115e0751ed00f31a629f35ba
BuddhikaKulasekara <kulasekara797@gmail.com>
2026-03-05 21:39:21 +0530

    remove comment

--- frontend/src/app/types/breadcrumb.ts
+++ frontend/src/app/types/breadcrumb.ts
@@ -2,7 +2,6 @@ export interface BreadcrumbProps {
   pageName: string;
   pageDescription?: string;
 }
-//beradcrumb link interface
 export interface BreadcrumbLink {
   href: string;
   text: string;
